20672ef0490604d6e2650b140278ea69aaef4478
Zorinfc <furkansarikaya1999@hotmail.com>
2023-11-14 17:45:52 +0300

    push 17:45

--- frontend/src/components/TodoInput/TodoInput.jsx
+++ frontend/src/components/TodoInput/TodoInput.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+
 // Bootstrap
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
@@ -8,11 +8,43 @@ import { Container } from 'react-bootstrap';
 // Iconu sonra eklemeye çalış
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBook } from '@fortawesome/free-solid-svg-icons';
+import React, { useState,useEffect } from 'react';
+import axios from 'axios';
+import TodoServicesApi from '../../services/TodoServicesApi';
 
 
+ function TodoInput () {
 
-export default class TodoInput extends Component {
-  render() {
+  const [newItem, setNewItem] = useState("");
+  const isItDone ='Yapılmadı'
+
+
+ 
+
+  //Create
+    const sendDataToApi = async()=>{
+      if(newItem !=""){
+        try{
+          const response = await fetch('http://localhost:9090/addTodo',{
+            method:'POST',
+            headers:{
+              'Content-Type':'application/json',
+            },
+            body:JSON.stringify({
+              status:isItDone,
+              title:newItem,
+            }),
+          });
+          const data = await response.json();
+          console.log('API Response:', data);
+          window.location.reload();
+        } catch (error) {
+          console.error('Error sending data:', error);
+      };
+      }
+      else window.alert("Boş değer girilemez");
+  }
+
     return (
       <Container className="px-5 mb-2 bg-secondary border rounded" >
       <h1 className='dispaly-1'>Input Todo</h1>
@@ -21,17 +53,22 @@ export default class TodoInput extends Component {
         <Form.Control
           placeholder="What todo"
           type="text"
+          value={newItem}
+          onChange={e=>setNewItem(e.target.value)}
         />
       </InputGroup>
       <div className="d-grid gap-2 mb-3">
       <Button 
         variant="btn btn-outline-dark" 
-        id="add-todo">
+        id="add-todo"
+        onClick={sendDataToApi}
+        >
           Add Todo
         </Button>   
       </div>
 
     </Container>
     );
-  }
+
 }
+export default TodoInput
--- frontend/src/components/TodoList/TodoList.jsx
+++ frontend/src/components/TodoList/TodoList.jsx
@@ -1,8 +1,153 @@
-import React from 'react'
-import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import TodoServicesApi from './../../services/TodoServicesApi'
+import axios from 'axios';
+import './todolist.css'
+
+
+
+
+
 function TodoList() {
+
+
+
+  const [todoListApi, setTodoListApi]=useState([]);
+
+  useEffect(() => {
+    fetchTodoList();
+  }, [])
+
+  const fetchTodoList = async () =>{
+    try{
+      const response = await TodoServicesApi.todoApiGetAllTodo();
+      if(response.status ===200){
+        setTodoListApi(response.data)
+      }
+    }catch(err){
+      console.log(err);
+    }
+  }
+
+  const setDeleteTodo = async (id) =>{
+    axios.delete("http://localhost:9090/deleteTodoById/"+id).then().catch();
+    console.log('Id li data silindi' + id);
+
+    // Hata verdi nedeninin bulamadım...
+    // window.location.reload();
+
+    setTimeout(() => {
+      // Sayfayı yenile
+      window.location.reload();
+    }, 1000);
+  }
+
+  //Update
+  // <-----------|YAPILACAK|----------->
+  const editTodo = (response)=>{
+    console.log(response.id);
+    console.log(response.title);
+    console.log(response.status);
+
+  }
+  
+  //CheckTodo
+  const checkTodoUpdate = async(response)=>{
+    let id = response.id
+    switch (response.status) {
+      case 'Yapıldı':
+        const newData1 = await fetch('http://localhost:9090/updateTodoById/'+id,{
+          method:'POST',
+          headers:{
+          'Content-Type':'application/json',
+          },
+          body:JSON.stringify({
+          status:'Yapılmadı',
+          title:response.title
+          }),
+          });
+          const data1 = await newData1.json();
+          console.log('Api response',data1);
+          window.location.reload();
+        break;
+      case 'Yapılmadı':
+          const newData = await fetch('http://localhost:9090/updateTodoById/'+id,{
+          method:'POST',
+          headers:{
+          'Content-Type':'application/json',
+          },
+          body:JSON.stringify({
+          status:'Yapıldı',
+          title:response.title
+          }),
+          });
+          const data = await newData.json();
+          console.log('Api response',data);
+          window.location.reload();
+        break;
+      default:
+        break;
+    }
+  }
+
+
+  // Tarih saat düzenlemesi
+  // Gelene data == "2023-11-14T14:20:28.663+00:00 "
+  const formattedDate =(date) => {
+    const isoDateString = date;
+    const MyDate = new Date(isoDateString);
+    //console.log(isoDateString);
+    const formattedDate = MyDate.toLocaleDateString('tr-TR', {
+      hour:'2-digit',
+      minute:'2-digit',
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric'
+        
+    });
+      //console.log(formattedDate);
+      return formattedDate;
+  }
+
   return (
-    <div>TodoList</div>
+    <React.Fragment>
+    <br /><br /><br /><br />
+    <table className='table table-dark'>
+      <thead className='thead-light'>
+        <tr>
+          <th>Title</th>
+          <th>Status</th>
+          <th>Date</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody className='tbody'>
+        {
+          todoListApi.map((response) =>
+            <tr key={response.id}>
+              <td>{response.title}</td>
+              <td>{response.status}</td>
+               {/*Formatlanmış tarih ve saat */}
+              <td>{formattedDate(response.date)}</td>
+              <td>
+                <div style={{cursor:'pointer'}}>
+                {/*Delete Button & Function*/}
+                <i onClick={() => setDeleteTodo(response.id)}
+                className="fa-solid fa-trash" ></i>
+                {/*Edit Button & Function*/}
+                <i onClick={()=> editTodo(response)}
+                className='fa-solid fa-pen-to-square'></i>
+                 {/*Check Button & Function*/}
+                <i onClick={() =>checkTodoUpdate(response)}
+                className='fa-solid fa-check'></i>
+                </div>
+              </td>
+            </tr>
+          )
+        }
+      </tbody>
+    </table>
+
+  </React.Fragment>
   )
 }
 
